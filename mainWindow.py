fbb1fe27c32d06362e659e91012ef2950207d899
a_ghoul <aaroncgould@gmail.com>
2019-06-10 14:07:46 -0600

    Slowly adding GUI compatability. I think I'm giving up on tabs because they are too confusing ):

--- mainWindow.py
+++ mainWindow.py
@@ -5,74 +5,25 @@ I might just start testing without GUI first though!!
 """
 
 import tkinter as tk
-import datetime
+from tkinter import ttk
+from classes import *
 
 
-class Entry:  # This is the entry for a switcheroo
-
-    def __init__(self, date, boxType, alternate1, alternate2, alternate3, name, phoneNumber):
-        self.date = date
-        self.boxType = boxType
-        self.alternateList = [alternate1, alternate2, alternate3]
-        self.name = name
-        self.phoneNumber = phoneNumber
-
-    def isMatch(self, entry):  # lol this isn't working. needs to check BOTH ways
-        matchingOneWay = False
-        for alternate in self.alternateList:
-            if alternate == entry.date:
-                matchingOneWay = True
-
-        matchingOtherWay = False
-        for alternate in entry.alternateList:
-            if alternate == self.date:
-                matchingOtherWay = True
-
-        matchingType = self.boxType == entry.boxType
-
-        return matchingOneWay and matchingOtherWay and matchingType
-
-
-class Entries:
-
-    entries = []
-
-    def addEntry(self, entry):
-        self.entries.append(entry)
-
-    def findMatches(self):
-        matches = []
-        for entry in self.entries:
-            for possibleMatch in self.entries:
-                if entry.isMatch(possibleMatch):
-                    matches.append(entry)
-        return matches
-
-
-class MainWindow:
+class MainWindow(tk.Tk):
 
     def __init__(self):
-        self.mainWin = tk.Tk()
-        self.mainWin.title("Switcheroo List")
+        super(MainWindow, self).__init__()
+        self.geometry("800x600")
+        self.title("Switcheroo List")
+        entry1 = Entry(datetime.datetime(2019, 6, 22), "Box", datetime.datetime(2019, 6, 15),
+                       datetime.datetime(2019, 6, 16), datetime.datetime(2019, 6, 17), "Bob", 1111111111)
+        self.displayEntry(entry1, 0)
 
-    def run(self):
-        self.mainWin.mainloop()
-
+    def displayEntry(self, entry, row):
+        resultsLabel = tk.Label(self, text=entry.name, font="Arial 11 bold", fg="black", borderwidth=5, bg="salmon")
+        resultsLabel.grid(column=1, row=row)
 
 if __name__ == "__main__":
     switcheroo = MainWindow()
-    # switcheroo.run()
+    switcheroo.mainloop()
 
-    entry1 = Entry(datetime.datetime(2019, 6, 22), "Box", datetime.datetime(2019, 6, 15),
-                   datetime.datetime(2019, 6, 16), datetime.datetime(2019, 6, 17), "Bob", 1111111111)
-
-    entry2 = Entry(datetime.datetime(2019, 6, 16), "Box", datetime.datetime(2019, 6, 21),
-                   datetime.datetime(2019, 6, 22), datetime.datetime(2019, 6, 24), "Mary", 2222222222)
-
-    print(entry1.isMatch(entry2))
-
-    entries = Entries()
-    entries.addEntry(entry1)
-    entries.addEntry(entry2)
-
-    print(entries.findMatches())
