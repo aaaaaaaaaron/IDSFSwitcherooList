c99f045a3ade3f5ec165907bc9267acd95323f29
a_ghoul <aaroncgould@gmail.com>
2019-06-10 19:07:49 -0600

    more stuff

--- mainWindow.py
+++ mainWindow.py
@@ -15,15 +15,35 @@ class MainWindow(tk.Tk):
         super(MainWindow, self).__init__()
         self.geometry("800x600")
         self.title("Switcheroo List")
-        entry1 = Entry(datetime.datetime(2019, 6, 22), "Box", datetime.datetime(2019, 6, 15),
-                       datetime.datetime(2019, 6, 16), datetime.datetime(2019, 6, 17), "Bob", 1111111111)
-        self.displayEntry(entry1, 0)
+
 
     def displayEntry(self, entry, row):
-        resultsLabel = tk.Label(self, text=entry.name, font="Arial 11 bold", fg="black", borderwidth=5, bg="salmon")
-        resultsLabel.grid(column=1, row=row)
+        entryLabel = tk.Label(self, text=entry.name, font="Arial 11 bold", fg="black", borderwidth=5, bg="salmon")
+        entryLabel.grid(column=1, row=row)
+
+    def loadEntries(self, entries):
+        row = 0
+        for entry in entries.entries:
+            self.displayEntry(entry, row)
+            print(entry.name)
+            row += 1
+
 
 if __name__ == "__main__":
     switcheroo = MainWindow()
-    switcheroo.mainloop()
 
+    entry1 = Entry(datetime.datetime(2019, 6, 22), "Box", datetime.datetime(2019, 6, 15),
+                   datetime.datetime(2019, 6, 16), datetime.datetime(2019, 6, 17), "Bob", 1111111111)
+
+    entry2 = Entry(datetime.datetime(2019, 6, 16), "Box", datetime.datetime(2019, 6, 21),
+                   datetime.datetime(2019, 6, 22), datetime.datetime(2019, 6, 24), "Mary", 2222222222)
+
+    testEntries = Entries()
+
+    testEntries.addEntry(entry1)
+    testEntries.addEntry(entry2)
+    print(testEntries.entries)
+
+    switcheroo.loadEntries(testEntries)
+
+    switcheroo.mainloop()
